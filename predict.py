5684c8c4e73cc58031820f59e88469dac4c02977
cicbeast <49789241+cicbeast@users.noreply.github.com>
2019-10-27 14:06:50 -0400

    Final edits made to notebooks, first tries at Flask app

--- predict.py
+++ predict.py
@@ -1,44 +1,59 @@
-# First let's import
-# Let's do our imports
+import pickle
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import math
-import pandas_profiling
 import category_encoders as ce
-from sklearn.model_selection import train_test_split, RandomizedSearchCV, GridSearchCV
+import itertools
+import warnings
+import sqlite3
+import requests
+import json
+from sklearn.neighbors import KNeighborsClassifier, NearestNeighbors
+from sklearn.model_selection import train_test_split
 from sklearn.pipeline import make_pipeline
-from xgboost import XGBRegressor, plot_importance
+from xgboost import XGBRegressor
+from sportsreference.nba.roster import Roster, Player
+from sportsreference.nba.player import AbstractPlayer
+from sportsreference.nba.teams import Teams
 from sklearn.metrics import r2_score, mean_squared_error
-from .nba import NBAPlayer
 
-%matplotlib inline
+
+teams = Teams()
+teamabbs = []
+squadnames = []
+for team in teams:
+    teamabbs.append(team.abbreviation)
+for abb in teamabbs:
+    squad = Roster(abb, slim=True)
+    squaddict = squad.players
+    squadnames.append(list(squaddict.values()))
+mergednames = list(itertools.chain.from_iterable(squadnames))
 
 rawpast = pd.read_csv("https://raw.githubusercontent.com/Build-Week-NBA-Longevity-Predictor/Data-Science/master/1976_to_2015_Draftees_edit2.csv")
-# Now we're going to get rid of the observations with 0 years, as those rows are inherently useless to us
 rawpast = rawpast[rawpast['Yrs'] != 0]
+past = rawpast[~rawpast['Player'].isin(mergednames)]
 
-trainval, test = train_test_split(rawpast, train_size=0.85, test_size=0.15, random_state=42)
-train, val = train_test_split(trainval, train_size=0.80, test_size=0.20, random_state=42)
+train, val = train_test_split(past, train_size=0.80, test_size=0.20,
+                              random_state=42)
 
-# With the information we get from the pandas profiler we can now organize our data
+
 def organize(X):
     X = X.copy()
     # Let's name some features that we don't need, Duplicates and Meaningless
     # and a few that would bleed into our model , Obvious
     duplicates = ['Win.Share', 'Unnamed: 0', 'Executive']
-    meaningless = ['Draft_Yr', 'first_year', 'second_year', 'third_year', 'fourth_year', 'fifth_year']
-    obvious = ['PTS', 'TRB', 'AST', 'Minutes.Played', 'Games' ]
-    
+    meaningless = ['Draft_Yr', 'first_year', 'second_year', 'third_year',
+                   'fourth_year', 'fifth_year']
+    obvious = ['PTS', 'TRB', 'AST', 'Minutes.Played', 'Games', 'VORP']
+
     # Now let's do a cosmetic change to College
     X['College'] = X['College'].replace('0', 'None')
-    
+
     # And we'll make Tenure(days) from the Tenure feature
     X['Tenure(days)'] = X['Tenure'].map(lambda x: str(x)[:-24]).astype('int64')
-    
+
     # We'll add Tenure to duplicates
     duplicates = duplicates + ['Tenure']
-    
+
     # Now let's drop the features we don't need
     todrop = duplicates + meaningless + obvious
     X = X.drop(columns=todrop)
@@ -48,22 +63,18 @@ def organize(X):
 
 train = organize(train)
 val = organize(val)
-test = organize(test)
-
 # Now let's arrange x matrix and y vector
 target = 'Yrs'
 xtrain = train.drop(columns=target)
 xval = val.drop(columns=target)
-xtest = test.drop(columns=target)
-
 ytrain = train[target]
 yval = val[target]
-ytest = test[target]
 
 feats = train.drop(columns=[target]).columns.tolist()
-
-# We'll drop some more features that won't match up with the API we useor it adds noise to the model
-todrop = {'Player', 'All_NBA', 'All.Star', 'College', 'Pk', 'Exec_ID', 'Exec_draft_exp', 'attend_college', 'Tenure(days)'}
+# We'll drop some more features that won't match up with the API we use
+# We'll also drom some more (post analysis) noisy features
+todrop = {'Player', 'All_NBA', 'All.Star', 'College', 'Pk', 'Exec_ID',
+          'Exec_draft_exp', 'attend_college', 'Tenure(days)'}
 feats = [e for e in feats if e not in todrop]
 
 
@@ -71,22 +82,61 @@ def rmse(ytrue, ypred):
     return np.sqrt(mean_squared_error(ytrue, ypred))
 
 
-# Now let's use XGBoost
-import warnings
-warnings.simplefilter(action='ignore', category=FutureWarning)
+# First we'll use the average NBA career length of 5 yrs
+avgyrs = 5
+ybase = np.full_like(yval, avgyrs)
+print('The Average Career of an NBA player is 5 Years')
+print(f"Baseline RMSE: {rmse(yval, ybase)}")
 
+# Now let's use XGBoost
+# We've already Optimized our Hyperparamaters
+warnings.simplefilter(action='ignore', category=FutureWarning)
 encoder = ce.OrdinalEncoder()
 xtrainencoded = encoder.fit_transform(xtrain[feats])
 xvalencoded = encoder.transform(xval[feats])
 eval_set = [(xtrainencoded, ytrain),
             (xvalencoded, yval)]
+xgbreg = XGBRegressor(n_estimators=586, max_depth=7, n_jobs=-1)
+xgbreg.fit(xtrainencoded, ytrain,
+           eval_set=eval_set, eval_metric='rmse', early_stopping_rounds=50)
+ypred = xgbreg.predict(xvalencoded)  # We can check this agains yval
 
-model = XGBRegressor(n_estimators=732, maxdepth=4 n_jobs=-1)
-model.fit(xtrainencoded, ytrain, 
-          eval_set=eval_set, eval_metric='rmse', early_stopping_rounds=50)
+# Now let's pickle our model
+pickle.dump(xgbreg, open('model.pkl', 'wb'))
 
+# Now that we have a model, let's make the test data convenient
+conn = sqlite3.connect('nbadb.sqlite3')
+cursor = conn.cursor()
+nbads = pd.read_sql("select * from nba_players;", conn)
+renamedict = {'name': 'Player', 'FG_Pct': 'FG_Percentage',
+              'TP_Pct': 'TP_Percentage', 'FT_Pct': 'FT_Percentage',
+              'Min_per_game': 'Minutes.per.Game',
+              'Pts_per_game': 'Points.per.Game', 'TRB_per_game': 'TRB.per.game',
+              'Asts_per_game': 'Assits.per.Game'}
+nbads = nbads.rename(columns=renamedict)
+reorderlist = ['Team', 'FG_Percentage', 'TP_Percentage', 'FT_Percentage',
+               'Minutes.per.Game', 'Points.per.Game', 'TRB.per.game',
+               'Assits.per.Game', 'WS_per_game', 'BPM', 'VORP', 'Player']
+nbads = nbads[reorderlist]
+# Now we'll set up a test player, Ben Simmons (nbads index 213)
+chkdata = nbads[nbads['Player'] == 'Ben Simmons']
+xtestencoded = encoder.transform(chkdata.drop(columns=['Player', 'VORP']))
 
+# Now let's load the model to find the result
+model = pickle.load(open('model.pkl', 'rb'))
+print('Predicted longevity of chosen player:', model.predict(xtestencoded))
 
+# Now let's find a comparable historic player
+Xpast = past[feats]
+coder = ce.OrdinalEncoder()
+Xencoded = coder.fit_transform(Xpast)
+nghbr = NearestNeighbors(n_neighbors=1, algorithm='auto').fit(Xencoded)
+distance, idx = nghbr.kneighbors(xtestencoded)
+finfeats = feats + ['Player', 'Yrs']
+comppast = past[finfeats]
+compplayer = comppast.iloc[index[0]]
+compresult = compplayer[['Player', 'Yrs']]
+print('A Comparable Historical Player is:', compresult)
 
-def predict_longevity(model, req_ref_id):
-    req_player = NBAPlayer.query.filter(NBAPlayer.ref_id == req_ref_id).one
+# Now let's pickle the Neighbor finder
+pickle.dump(nghbr, open('compare.pkl', 'wb'))
