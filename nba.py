6e4fb8c2f3a8c0ef858c109078d42966fc4aeec1
cicbeast <49789241+cicbeast@users.noreply.github.com>
2019-10-27 23:19:58 -0400

    Edits to nba.py and Model.ipynb

--- nba.py
+++ nba.py
@@ -1,6 +1,6 @@
 import pickle
 import pandas as pd
-from .predict import encoder, coder, nbads
+from predict import encoder, coder, nbads
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sportsreference.nba.roster import Roster, Player
@@ -14,21 +14,20 @@ APP.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 DB = SQLAlchemy(APP)
 
 
-@APP.route('/')
-def root():
-    return "Hi"
-
-
-@APP.route('/predict', methods=['GET', 'POST'])
-def predict():
+@APP.route('/', methods=['GET', 'POST'])
+def prediction():
     model = pickle.load(open('model.pkl', 'rb'))
     compare = pickle.load(open('compare.pkl', 'rb'))
     askname = request.get_json(force=True, silent=True)
     chkdata = nbads[nbads['Player'] == askname]
     xtestencoded = encoder.transform(chkdata.drop(columns=['Player', 'VORP']))
     longevity = model.predict(xtestencoded)
-    output1 = longevity[0]
+    output1 = {longevity[0]}
     distances, index = compare.kneighbors(xtestencoded)
-    comparable = comppast.iloc[idx[0]]
-    output2 = comparable[['Player', 'Yrs']]
+    comparable = comppast.iloc[index[0]]
+    output2 = {comparable[['Player', 'Yrs']]}
     return jsonify(output1, output2)
+
+
+if __name__ == '__main__':
+    APP.run(port = 5000, debug=True)
